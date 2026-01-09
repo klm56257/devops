82de1181bd047ba89eb471f2347bf42a66c209d1
Liam <liphommasone@stpbb.org>
2026-01-09 14:37:02 +0100

    ss

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -10,5 +11,29 @@ class HelloTest extends TestCase
     {
         $hello = new Hello();
         $this->assertSame('Bonjour, BTS SIO SLAM!', $hello->sayHello('BTS SIO SLAM'));
+    }
+
+    public function testSayHelloEmpty(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, !', $hello->sayHello(''));
+    }
+
+    public function testSpecialCharacters(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, Élodie!', $hello->sayHello('Élodie'));
+    }
+
+    public function testNumericName(): void
+    {
+        $hello = new Hello();
+        $this->assertSame('Bonjour, 123!', $hello->sayHello('123'));
+    }
+
+    public function testReturnType(): void
+    {
+        $hello = new Hello();
+        $this->assertIsString($hello->sayHello('Bob'));
     }
 }
